6fff0c280c767c1af6cc7d0d1bca1d5400ab1b87
anthonygone <161100797+anthonygone@users.noreply.github.com>
2026-06-22 15:47:11 +0530

    Update ConnectWallet.tsx

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -13,7 +13,7 @@ export function ConnectWallet() {
         {connectors.map((connector) => (
           <button
             key={connector.uid}
-            onClick={() => connect({ connector })}
+            onClick={() => connect({ connect })}
             disabled={isConnecting}
             className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
           >
