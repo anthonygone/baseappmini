84ea70d729e540fd979b72a89d064192a80a2cc0
anthonygone <161100797+anthonygone@users.noreply.github.com>
2026-06-26 15:26:50 +0530

    Update ConnectWallet.tsx

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -3,7 +3,7 @@
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
 
 export function ConnectWallet() {
-  const { isaddress, isConnected, Connecting } = useAccount()
+  const { isaddress, isConnected, isConnecting } = useAccount()
   const { connect, connectors } = useConnect()
   const { disconnect } = useDisconnect()
 
